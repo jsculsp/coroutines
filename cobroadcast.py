0a373b0a06cc5e43298975ac96b184d1f2345578
jsculsp <328232234@qq.com>
2017-04-19 16:16:53 +0800

    save

--- cobroadcast.py
+++ cobroadcast.py
@@ -8,31 +8,39 @@ from coroutine import coroutine
 # data into one (target)
 
 import time
+
+
 def follow(thefile, target):
-    thefile.seek(0,2)      # Go to the end of the file
+    thefile.seek(0, 2)      # Go to the end of the file
     while True:
-         line = thefile.readline()
-         if not line:
-             time.sleep(0.1)    # Sleep briefly
-             continue
-         target.send(line)
+        line = thefile.readline()
+        if not line:
+            time.sleep(0.1)    # Sleep briefly
+            continue
+        target.send(line)
 
 # A filter.
+
+
 @coroutine
-def grep(pattern,target):
+def grep(pattern, target):
     while True:
         line = (yield)           # Receive a line
         if pattern in line:
             target.send(line)    # Send to next stage
 
 # A sink.  A coroutine that receives data
+
+
 @coroutine
 def printer():
     while True:
-         line = (yield)
-         print line,
+        line = (yield)
+        print line,
 
 # Broadcast a stream onto multiple targets
+
+
 @coroutine
 def broadcast(targets):
     while True:
@@ -44,7 +52,7 @@ def broadcast(targets):
 if __name__ == '__main__':
     f = open("access-log")
     follow(f,
-       broadcast([grep('python',printer()),
-                  grep('ply',printer()),
-                  grep('swig',printer())])
+           broadcast([grep('python', printer()),
+                      grep('ply', printer()),
+                      grep('swig', printer())])
            )
