d45e771d674cdc659358d3f3a5a51b5e699d38c6
jsculsp <328232234@qq.com>
2017-04-19 15:53:45 +0800

    save

--- coroutine.py
+++ coroutine.py
@@ -3,9 +3,10 @@
 # A decorator function that takes care of starting a coroutine
 # automatically on call.
 
+
 def coroutine(func):
-    def start(*args,**kwargs):
-        cr = func(*args,**kwargs)
+    def start(*args, **kwargs):
+        cr = func(*args, **kwargs)
         cr.next()
         return cr
     return start
