53ccd366c235d964b78e2e7008a80bdc6d1ac11e
jsculsp <328232234@qq.com>
2017-04-25 17:26:25 +0800

    save

--- cosax.py
+++ cosax.py
@@ -4,15 +4,19 @@
 
 import xml.sax
 
+
 class EventHandler(xml.sax.ContentHandler):
-    def __init__(self,target):
+    def __init__(self, target):
         self.target = target
-    def startElement(self,name,attrs):
-        self.target.send(('start',(name,attrs._attrs)))
-    def characters(self,text):
-        self.target.send(('text',text))
-    def endElement(self,name):
-        self.target.send(('end',name))
+
+    def startElement(self, name, attrs):
+        self.target.send(('start', (name, attrs._attrs)))
+
+    def characters(self, text):
+        self.target.send(('text', text))
+
+    def endElement(self, name):
+        self.target.send(('end', name))
 
 # example use
 if __name__ == '__main__':
