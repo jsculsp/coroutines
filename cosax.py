7e927065e79ff01648c52c6ce1af42c9f7a86461
jsculsp <328232234@qq.com>
2017-04-26 09:37:37 +0800

    save

--- cosax.py
+++ cosax.py
@@ -10,7 +10,7 @@ class EventHandler(xml.sax.ContentHandler):
         self.target = target
 
     def startElement(self, name, attrs):
-        self.target.send(('start', (name, attrs)))
+        self.target.send(('start', (name, attrs._attrs)))
 
     def characters(self, text):
         self.target.send(('text', text))
